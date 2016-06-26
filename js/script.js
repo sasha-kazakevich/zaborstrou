e2a6eac4b94cdd4f9906632ebe7ce5f032a40d41
Sasha Kazakevich <ssasshaka@gmail.com>
2016-06-26 23:39:52 +0300

    all page

--- js/script.js
+++ js/script.js
@@ -1,1 +1,46 @@
-console.log("Ky!");
+$(document).ready(function(){
+  $(function () {
+	var elWrap = $('#slider'),
+		el =  elWrap.find('img'),
+		indexImg = 1,
+		indexMax = el.length;
+
+	function change () {
+		el.fadeOut(500);
+		el.filter(':nth-child('+indexImg+')').fadeIn(500);
+	}
+
+	function autoCange () {
+		indexImg++;
+		if(indexImg > indexMax) {
+			indexImg = 1;
+		}
+		change ();
+	}
+	var interval = setInterval(autoCange, 5000);
+
+	elWrap.mouseover(function() {
+		clearInterval(interval);
+	});
+	elWrap.mouseout(function() {
+		interval = setInterval(autoCange, 5000);
+	});
+
+	elWrap.append('<span class="next"></span><span class="prev"></span>');
+
+	$('span.next').click(function() {
+		indexImg++;
+		if(indexImg > indexMax) {
+			indexImg = 1;
+		}
+		change ();
+	});
+	$('span.prev').click(function() {
+		indexImg--;
+		if(indexImg < 1) {
+			indexImg = indexMax;
+		}
+		change ();
+	});
+});
+})
